79aaae5b41e210150aa2c55c2fbacd1197de6fc9
Sven Anders <github-commit@sven.anders.im>
2018-02-11 17:11:45 +0100

    Add User Controler

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ radschulwegplan-backend
 =======================
 
 A Symfony project created on February 11, 2018, 4:20 pm.
+
+Vorlage:  https://www.cloudways.com/blog/rest-api-in-symfony-3-1/
---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,27 @@
+<?php
+namespace AppBundle\Controller;
+ 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
+use AppBundle\Entity\User;
+
+class UserController extends FOSRestController
+{
+
+    /**
+     * @Rest\Get("/api/v1/user")
+     */
+    public function getAction()
+    {
+      $restresult = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
+        if ($restresult === null) {
+          return new View("there are no users exist", Response::HTTP_NOT_FOUND);
+     }
+        return $restresult;
+    }
+}
