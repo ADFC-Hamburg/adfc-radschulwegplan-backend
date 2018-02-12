4ccaf6c6418cb43895c0e7fdfd2d22187146b7df
Sven Anders <github-commit@sven.anders.im>
2018-02-12 18:03:54 +0100

    change SRID and move to BaseEntity

---
+++ src/AppBundle/Entity/BaseEntity.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * BaseEntity
+ *
+ */
+abstract class BaseEntity
+{
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    protected $created;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="changed", type="datetime")
+     */
+    protected $changed;
+
+    /**
+     * @var int
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="created_by", referencedColumnName="id")
+     */
+    protected $createdBy;
+
+    /**
+     * @var int
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="changed_by", referencedColumnName="id")
+     */
+    protected $changedBy;
+
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return BaseEntity
+     */
+    public function setCreated(DateTime $created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated():DateTime
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set changed
+     *
+     * @param \DateTime $changed
+     *
+     * @return BaseEntity
+     */
+    public function setChanged(DateTime $changed)
+    {
+        $this->changed = $changed;
+
+        return $this;
+    }
+
+    /**
+     * Get changed
+     *
+     * @return \DateTime
+     */
+    public function getChanged()
+    {
+        return $this->changed;
+    }
+
+    /**
+     * Set createdBy
+     *
+     * @param integer $createdBy
+     *
+     * @return BaseEntity
+     */
+    public function setCreatedBy(User $createdBy)
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return User
+     */
+    public function getCreatedBy():User
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * Set changedBy
+     *
+     * @param integer $changedBy
+     *
+     * @return BaseEntity
+     */
+    public function setChangedBy(User $changedBy)
+    {
+        $this->changedBy = $changedBy;
+
+        return $this;
+    }
+
+    /**
+     * Get changedBy
+     *
+     * @return User
+     */
+    public function getChangedBy():User
+    {
+        return $this->changedBy;
+    }
+}
+
--- src/AppBundle/Entity/DangerPoint.php
+++ src/AppBundle/Entity/DangerPoint.php
@@ -10,8 +10,15 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="danger_point")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\DangerPointRepository")
  */
-class DangerPoint
+class DangerPoint extends BaseEntity
 {
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+
     /**
      * @var int
      *
@@ -24,7 +31,7 @@ class DangerPoint
     /**
      * @var geometry
      *
-     * @ORM\Column(name="pos", type="geometry",options={"geometry_type"="POINT", "srid"=3785})
+     * @ORM\Column(name="pos", type="geometry",options={"geometry_type"="POINT", "srid"=4326})
      */
     private $pos;
 
@@ -48,37 +55,6 @@ class DangerPoint
      * @ORM\Column(name="typeId", type="integer")
      */
     private $typeId;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="created", type="datetime")
-     */
-    private $created;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="changed", type="datetime")
-     */
-    private $changed;
-
-    /**
-     * @var int
-     *
-     * @ORM\ManyToOne(targetEntity="User")
-     * @ORM\JoinColumn(name="created_by", referencedColumnName="id")
-     */
-    private $createdBy;
-
-    /**
-     * @var int
-     *
-     * @ORM\ManyToOne(targetEntity="User")
-     * @ORM\JoinColumn(name="changed_by", referencedColumnName="id")
-     */
-    private $changedBy;
-
 
     /**
      * Get id
@@ -186,100 +162,5 @@ class DangerPoint
         return $this->typeId;
     }
 
-    /**
-     * Set created
-     *
-     * @param \DateTime $created
-     *
-     * @return DangerPoint
-     */
-    public function setCreated($created)
-    {
-        $this->created = $created;
-
-        return $this;
-    }
-
-    /**
-     * Get created
-     *
-     * @return \DateTime
-     */
-    public function getCreated()
-    {
-        return $this->created;
-    }
-
-    /**
-     * Set changed
-     *
-     * @param \DateTime $changed
-     *
-     * @return DangerPoint
-     */
-    public function setChanged($changed)
-    {
-        $this->changed = $changed;
-
-        return $this;
-    }
-
-    /**
-     * Get changed
-     *
-     * @return \DateTime
-     */
-    public function getChanged()
-    {
-        return $this->changed;
-    }
-
-    /**
-     * Set createdBy
-     *
-     * @param integer $createdBy
-     *
-     * @return DangerPoint
-     */
-    public function setCreatedBy($createdBy)
-    {
-        $this->createdBy = $createdBy;
-
-        return $this;
-    }
-
-    /**
-     * Get createdBy
-     *
-     * @return int
-     */
-    public function getCreatedBy()
-    {
-        return $this->createdBy;
-    }
-
-    /**
-     * Set changedBy
-     *
-     * @param integer $changedBy
-     *
-     * @return DangerPoint
-     */
-    public function setChangedBy($changedBy)
-    {
-        $this->changedBy = $changedBy;
-
-        return $this;
-    }
-
-    /**
-     * Get changedBy
-     *
-     * @return int
-     */
-    public function getChangedBy()
-    {
-        return $this->changedBy;
-    }
 }
 
