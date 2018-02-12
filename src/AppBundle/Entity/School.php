35d451cb2b6349e95abb2167d7c63eeb88f90c43
Sven Anders <github-commit@sven.anders.im>
2018-02-12 11:31:22 +0100

    New Entity School

---
+++ src/AppBundle/Entity/School.php
@@ -0,0 +1,292 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * School
+ *
+ * @ORM\Table(name="school")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\SchoolRepository")
+ */
+class School
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="street", type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="postalcode", type="string", length=5)
+     */
+    private $postalcode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="place", type="string", length=64)
+     */
+    private $place;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="webpage", type="string", length=255, nullable=true)
+     */
+    private $webpage;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="last_change", type="datetime")
+     */
+    private $lastChange;
+
+    /**
+     * @var int
+     *
+     * @ManyToOne(targetEntity="User")
+     * @JoinColumn(name="changed_by", referencedColumnName="id")
+     */
+    private $changedBy;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var int
+     *
+     * @ManyToOne(targetEntity="User")
+     * @JoinColumn(name="created_by", referencedColumnName="id")
+     */
+    private $createdBy;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return School
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set street
+     *
+     * @param string $street
+     *
+     * @return School
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    /**
+     * Get street
+     *
+     * @return string
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set postalcode
+     *
+     * @param string $postalcode
+     *
+     * @return School
+     */
+    public function setPostalcode($postalcode)
+    {
+        $this->postalcode = $postalcode;
+
+        return $this;
+    }
+
+    /**
+     * Get postalcode
+     *
+     * @return string
+     */
+    public function getPostalcode()
+    {
+        return $this->postalcode;
+    }
+
+    /**
+     * Set place
+     *
+     * @param string $place
+     *
+     * @return School
+     */
+    public function setPlace($place)
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+
+    /**
+     * Get place
+     *
+     * @return string
+     */
+    public function getPlace()
+    {
+        return $this->place;
+    }
+
+    /**
+     * Set webpage
+     *
+     * @param string $webpage
+     *
+     * @return School
+     */
+    public function setWebpage($webpage)
+    {
+        $this->webpage = $webpage;
+
+        return $this;
+    }
+
+    /**
+     * Get webpage
+     *
+     * @return string
+     */
+    public function getWebpage()
+    {
+        return $this->webpage;
+    }
+
+    /**
+     * Set lastChange
+     *
+     * @param \DateTime $lastChange
+     *
+     * @return School
+     */
+    public function setLastChange($lastChange)
+    {
+        $this->lastChange = $lastChange;
+
+        return $this;
+    }
+
+    /**
+     * Get lastChange
+     *
+     * @return \DateTime
+     */
+    public function getLastChange()
+    {
+        return $this->lastChange;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return School
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set createdBy
+     *
+     * @param integer $createdBy
+     *
+     * @return School
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return int
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+}
+
