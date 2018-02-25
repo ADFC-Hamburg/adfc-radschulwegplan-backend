47d60ff462fe63aeba6040ad6395acdf1817bc8c
Sven Anders <github-commit@sven.anders.im>
2018-02-25 09:28:14 +0100

    #3: test insert, get and delete methods

--- tests/AppBundle/Controller/DangerPointTest.php
+++ tests/AppBundle/Controller/DangerPointTest.php
@@ -2,10 +2,12 @@
 
 namespace Tests\AppBundle\Controller;
 
-
 class DangerPointTest extends BaseTestCase
 {
     const GET_ALL_API_PATH="/api/v1/danger_point";
+    const INSERT_API_PATH="/api/v1/danger_point/";
+    const GET_ONE_API_PREFX="/api/v1/danger_point/";
+    const DELETE_API_PREFX="/api/v1/danger_point/";
     
     public function testLogin() {
         $client=$this->createAdminAuthorizedClient();
@@ -15,14 +17,15 @@ class DangerPointTest extends BaseTestCase
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
 
-    public function testGetNoAccessWithoutLogin() {
+    public function testGetAllNoAccessWithoutLogin() {
         $client = static::createClient();
         $crawler = $client->request('GET', self::GET_ALL_API_PATH);
         $this->assertEquals(302,$client->getResponse()->getStatusCode());
         $this->assertContains('/login',$client->getResponse()->getTargetUrl());
     }
 
-    public function testGetWithLogin() {
+
+    public function testGetAll() {
         $client = $this->createAdminAuthorizedClient();
         $crawler = $client->request('GET', self::GET_ALL_API_PATH);
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
@@ -34,5 +37,59 @@ class DangerPointTest extends BaseTestCase
             $this->assertEquals($i, $data[$i]['type_id']);
         }
     }
+
+    public function testInsert() {
+        $client = $this->createAdminAuthorizedClient();
+        $crawler = $client->request(
+            'POST',
+            self::INSERT_API_PATH,
+            array(
+                'lat' => 53.51142,
+                'lon' => 10.101,
+                'title' => 'Bordstein zu hoch',
+                'description' => 'Der Bordstein ist hier 50 cm',
+                'typeId' => 9
+            )
+        );
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $data = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('id', $data);
+        $this->assertEquals(9, $data['type_id']);
+        $this->assertEquals('SRID=4326;POINT(53.511420 10.101000)', $data['pos']);
+        $this->assertEquals('Bordstein zu hoch', $data['title']);
+        $this->assertEquals('Der Bordstein ist hier 50 cm', $data['description']);
+        $this->assertEquals('test-admin', $data['created_by']['username']);
+        $this->assertEquals('test-admin', $data['changed_by']['username']);
+
+        $client = $this->createAdminAuthorizedClient();
+        $olddata= $data;
+        $crawler = $client->request(
+            'GET',
+            self::GET_ONE_API_PREFX.$olddata['id']);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $data = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals($olddata['id'], $data['id']);
+        $this->assertEquals(9, $data['type_id']);
+        $this->assertEquals('SRID=4326;POINT(53.51142 10.101)', $data['pos']);
+        $this->assertEquals('Bordstein zu hoch', $data['title']);
+        $this->assertEquals('Der Bordstein ist hier 50 cm', $data['description']);
+        $this->assertEquals('test-admin', $data['created_by']['username']);
+        $this->assertEquals('test-admin', $data['changed_by']['username']);
+        $crawler = $client->request(
+            'DELETE',
+            self::DELETE_API_PREFX.$olddata['id']);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $crawler = $client->request(
+            'DELETE',
+            self::DELETE_API_PREFX.$olddata['id']);
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $crawler = $client->request(
+            'GET',
+            self::GET_ONE_API_PREFX.$olddata['id']);
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        
+        
+    }
+
 
 }
